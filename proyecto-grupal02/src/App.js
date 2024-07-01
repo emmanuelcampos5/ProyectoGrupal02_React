c3b9ddb5b0fb015184ca75ff20728701db357a3c
emmanuelcampos5 <emmanuelcampos2111@gmail.com>
2024-07-01 00:24:43 -0600

    card creada

--- proyecto-grupal02/src/App.js
+++ proyecto-grupal02/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div>   
-      <div>   
-        <Menu/>
+    <div>
+      <div>
+        <Menu />
       </div>
-      <div className="container-fluid">           
-        {/* { activeComponent === 'curso' && <CursoList/>}
-        { activeComponent === 'grupo' && <GrupoList/>} */}
-        {/* <CursoList/>
-        <GrupoList/> */}
-      </div>      
-      <div className="container-fluid">   
-        {/* <Footer/> */}
+      <div className='container mt-4'>
+        {/* Este div es el que crea por cada publicacion */}
+        <div className="cardPost">
+          <img src="/fondo.jpg" className="card-img-top" alt="..."></img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
-    </div>  
+    </div>
   );
 }
 
